9a74db9bc3eaf5b24638b6e0cc8c8708d59211cd
DiegoFdezAb <114830990+DiegoFdezAb@users.noreply.github.com>
2024-05-09 17:19:51 +0200

    Ajoutez des fichiers projet.

---
+++ README.md
@@ -0,0 +1,17 @@
+# MacroDeck 2 Volume Mixer Plugin
+
+## Description
+The MacroDeck Volume Mixer plugin is designed to allow you to control the volume of your system using your MacroDeck keyboard. It will enable you to adjust individual application volumes, and create custom volume profiles.
+
+## Features
+- Control individual application volumes
+- Create custom volume profiles
+
+## Contributions - LATER
+Contributions to the project are welcome! If you would like to contribute, please refer to the CONTRIBUTING.md file for detailed instructions.
+
+## Library
+This plugin uses the [NAudio](https://github.com/naudio/NAudio) library.
+
+## Compatibility
+This plugin is only compatible with Macro Deck 2.
---
+++ VolumeMixerPlugin/Models/VolumeAppActionConfigModel.cs
@@ -0,0 +1,33 @@
+﻿using SuchByte.MacroDeck.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace VolumeMixerPlugin.Models
+{
+    internal class VolumeAppActionConfigModel : ISerializableConfiguration
+    {
+        public string AppName { get; set; } = String.Empty;
+        public int Volume { get; set; } = 0;
+
+        public string Serialize()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static VolumeAppActionConfigModel Deserialize(string value)
+        {
+            return ISerializableConfiguration.Deserialize<VolumeAppActionConfigModel>(value);
+        }
+
+        /*public void Deserialize(string value)
+        {
+            string[] values = value.Split(';');
+            AppName = values[0];
+            Volume = int.Parse(values[1]);
+        }*/
+    }
+}
